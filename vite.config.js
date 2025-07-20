e03d39943bed55ba5366f734eb670ff88c039b90
재진 오 <zldlxkdk@gmail.com>
2025-07-20 12:44:21 +0900

    Revert "GitHub Pages 배포 설정 추가 - 404 에러 해결"
    This reverts commit 158e14e9b1911c1a504807f056d74eeb9bf54565.

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ARCONVERTOR/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          tensorflow: ['@tensorflow/tfjs', '@tensorflow/tfjs-backend-webgl'],
-          mui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled']
-        }
-      }
-    }
-  }
 })
